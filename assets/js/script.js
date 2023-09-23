4788c3a2017002d85ddd1ccd117f1c38be7d2f22
Kevin <125530125+cheerfulman1014@users.noreply.github.com>
2023-09-23 05:08:03 +0300

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,120 @@
+"use strict";
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+navToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navToggleBtn);
+  elemToggleFunc(navbar);
+  elemToggleFunc(document.body);
+});
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) {
+      elemToggleFunc(toggleBtns[i]);
+    }
+    elemToggleFunc(skillsBox);
+  });
+}
+
+/**
+ * dark & light theme toggle
+ */
+
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+
+themeToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(themeToggleBtn);
+
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+
+    localStorage.setItem("theme", "light_theme");
+  } else {
+    document.body.classList.add("dark_theme");
+    document.body.classList.remove("light_theme");
+
+    localStorage.setItem("theme", "dark_theme");
+  }
+});
+
+/**
+ * check & apply last time selected theme from localStorage
+ */
+
+if (localStorage.getItem("theme") === "light_theme") {
+  themeToggleBtn.classList.add("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
+} else {
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("light_theme");
+  document.body.classList.add("dark_theme");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var formData = new FormData(this);
+
+  // Using Formspree's URL here
+  fetch("https://formspree.io/f/xyyqnlgb", {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json", // Indicating that we expect a JSON response
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      // You can customize this based on the response from Formspree if needed
+      if (result.ok) {
+        alert("Message sent successfully!");
+      } else {
+        alert("There was an error sending your message. Please try again.");
+      }
+    })
+    .catch((error) => {
+      alert("There was an error sending your message. Please try again.");
+    });
+});
